5bf0eb512ed52d80fbf9b004686997371fd49444
pritam guha <pritam@tenxerlabs.com>
2024-06-12 21:30:15 +0530

    Added comments and documentetion

---
+++ comments_&_documentation(chapter-2).py
@@ -0,0 +1,25 @@
+# COMMENTS -- (Use case)
+#  1. Comments can be used to explain Python code.
+#  2. Comments can be used to make the code more readable.
+#  3. Comments can be used to understand our code to others.
+
+# 1. Single Line comments
+# print('Hello Pritam')
+
+# 2. Inline comments
+print('What is your name?')  # This line is printing
+
+# 3. Multiline comments
+"""
+[Info] -- This is giving some information
+This is giving some information.
+"""
+print('This is giving information')
+
+# Documentation using docstring
+# 1. Comments must be the first statement of the component they describe.
+
+"""
+[Info] -- This is docstring.
+This is giving some information.
+"""
