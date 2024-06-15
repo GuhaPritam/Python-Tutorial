001f9b6de0212af4a25933d8c3636e3ac9b1f898
pritam guha <pritam@tenxerlabs.com>
2024-06-15 22:24:44 +0530

    Added data types

---
+++ data_types_(chapter-4).py
@@ -0,0 +1,25 @@
+"""
+    Data Types
+    ------------------------
+1. Numeric Types :-
+    1. int    2. float    3.complex    4.long
+
+2. Sequence Types :-
+    1. str    2. list    3.tuple    4.range
+
+3. Mapping Type :-
+    1. dict
+
+4. Set Types :-
+    1. set    2.frozenset
+
+5. Boolean Type :-
+    1. bool
+
+6. Binary Types :-
+    1. bytes    2.bytearray    3.memoryview
+
+7. None Type :-
+    1. NoneType
+"""
+
