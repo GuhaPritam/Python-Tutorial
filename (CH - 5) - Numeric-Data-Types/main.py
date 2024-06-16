90d38f1a2fb92458152a81440f9433f28b267d3b
pritam guha <pritam@tenxerlabs.com>
2024-06-16 15:16:24 +0530

    added numeric data type

---
+++ (CH - 5) - Numeric-Data-Types/main.py
@@ -0,0 +1,19 @@
+"""
+    Data Types
+    ------------------------
+1. Numeric Types :-
+    1. int    2. float    3.complex    4.long
+
+"""
+integer = 10
+print(type(integer))
+
+float = 20.3
+print(type(float))
+
+complex = 10 + 2j
+print(type(complex))
+
+
+print(float * integer)
+
