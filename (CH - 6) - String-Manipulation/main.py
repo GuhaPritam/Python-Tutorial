549ddc357c3a81d8db6b5ae0e3f94ddd0de7ced4
pritam guha <pritam@tenxerlabs.com>
2024-06-18 20:54:08 +0530

    string manupulation and some operations

---
+++ (CH - 6) - String-Manipulation/main.py
@@ -0,0 +1,12 @@
+"""
+String is immutable
+"""
+txt = 'My name is Pritam'
+print(len(txt))
+print('Pritam' in txt)
+print('hello' not in txt)
+
+name = 'Pritam'
+age = 24
+print(f'My name is {name} and my age is {age}') # fstring
+print('My name is {} and my age is {}'.format(name, age)) # formatting
