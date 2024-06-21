d71718d61cbaeb5ee6db5835e619e531c14649db
pritam guha <pritam@tenxerlabs.com>
2024-06-21 10:30:20 +0530

    Added string slicing

---
+++ (CH - 6) - String-Manipulation/(CH - 6.1) - slicing.py
@@ -0,0 +1,14 @@
+"""
+String Slicing
+
+string[start index : end index : steps] (indexing operator = [])
+
+Definition :-
+     In Python, [] (square brackets) are referred to as the indexing operator or subscript operator. They are used to
+     access specific elements or parts of various data structures such as lists, tuples, strings, etc.
+
+"""
+data = '01234567'
+print(data[2:5])
+print(data[-6:-3])
+print(data[::3])
--- (CH - 6) - String-Manipulation/main.py
+++ (CH - 6) - String-Manipulation/main.py
@@ -8,5 +8,5 @@ print('hello' not in txt)
 
 name = 'Pritam'
 age = 24
-print(f'My name is {name} and my age is {age}') # fstring
-print('My name is {} and my age is {}'.format(name, age)) # formatting
+print(f'My name is {name} and my age is {age}')  # fstring
+print('My name is {} and my age is {}'.format(name, age))  # formatting
