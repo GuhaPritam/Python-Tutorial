a9206912cacbf7e479306cffd6fa785cc82299f8
pritam guha <pritam@tenxerlabs.com>
2024-06-26 19:19:09 +0530

    Added concatination and formating.

---
+++ (CH - 6) - String-Manipulation/(CH - 6.2) - concatenation_&_formatting.py
@@ -0,0 +1,16 @@
+string_one = "Hello"
+string_two = "Pritam"
+new_string = string_one + " " + string_two
+print(new_string)
+
+list = ["Join", "Method"]
+method_two = " ".join(list)
+print(method_two)
+
+name = "Pritam"
+age = 26
+f_string = f"My name is {name} and I am {age} years old."
+print(f_string)
+formating = "My name is {} and I am {} years old.".format(name, age)
+print(formating)
+
